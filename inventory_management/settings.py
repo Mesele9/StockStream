863ec18abcef6984e14fee96a42442b1e51370e3
Mesele Assefa <getmesy@gmail.com>
2024-06-15 01:26:41 +0000

    add bulk import to admin

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+
+    'import_export',
+    
+    'store',
 ]
 
 MIDDLEWARE = [
--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,20 @@
 
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
 from .models import Item, PurchaseRecord, PurchaseRecordItem, IssueRecord, IssueRecordItem
+
+class ItemResource(resources.ModelResource):
+    class Meta:
+        model = Item
+        fields = ('id', 'description', 'category', 'unit_of_measurement', 'current_unit_price', 'stock_balance', 'minimum_stock')
+
+@admin.register(Item)
+class ItemAdmin(ImportExportModelAdmin):
+    resource_class = ItemResource
+    list_display = ('description', 'category', 'unit_of_measurement', 'current_unit_price', 'stock_balance', 'minimum_stock')
+    search_fields = ('description', 'category')
+
 
 class PurchaseRecordItemInline(admin.TabularInline):
     model = PurchaseRecordItem
@@ -9,7 +23,7 @@ class PurchaseRecordItemInline(admin.TabularInline):
 class PurchaseRecordAdmin(admin.ModelAdmin):
     inlines = [PurchaseRecordItemInline]
 
-admin.site.register(Item)
+
 admin.site.register(PurchaseRecord, PurchaseRecordAdmin)
 admin.site.register(IssueRecord)
 
